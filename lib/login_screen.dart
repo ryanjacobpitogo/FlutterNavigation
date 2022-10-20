e1bb95af0d9678a74ca698874bcdb6cf0d09a9cf
ryanajacobpitogo <ryanjacob.pitogo.20@usjr.edu.ph>
2022-10-21 05:10:00 +0800

    Final commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -6,6 +6,7 @@ class LoginScreen extends StatefulWidget {
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
+
 }
 
 class _LoginScreenState extends State<LoginScreen> {
@@ -88,7 +89,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           padding: const EdgeInsets.only(left: 20),
                           child: TextField(
                             obscureText: true,
-                            decoration: InputDecoration(
+                            decoration: InputDecoration(  
                               border: InputBorder.none,
                               hintText: 'Password',
                               hintStyle: GoogleFonts.raleway(
@@ -103,20 +104,28 @@ class _LoginScreenState extends State<LoginScreen> {
                 const SizedBox(height: 15),  
                 //login button
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.brown,
-                      borderRadius: BorderRadius.circular(15)
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                          MaterialPageRoute(builder: (context) => const ScreensPitogo1())
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.brown,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
                     ),
-                    child: Center(
-                      child: Text(
-                        'Log In',
-                        style: GoogleFonts.lato(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Text(
+                          'Log In',
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                          ),
                         ),
                       ),
                     ),
@@ -138,7 +147,6 @@ class _LoginScreenState extends State<LoginScreen> {
                          color: Colors.brown
                       ),
                     ),
-                    
                   ],
                 )
                 ]
@@ -147,6 +155,323 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
       ),
+    );
+  }
+}
+
+
+class ScreensPitogo1 extends StatelessWidget {
+  const ScreensPitogo1({super.key});
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Column( 
+                children:[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+                    child: Container(
+                      height: 125,
+                      width: double.infinity,
+                      color : Colors.grey[400]
+                    ),
+                  ), 
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+                    child: Container(
+                      height: 125,
+                      width: double.infinity,
+                      color : Colors.grey[600]
+                    ),
+                  ), 
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+                    child: Container(
+                      height: 125,
+                      width: double.infinity,
+                      color : Colors.grey[600]
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20.0),
+                    child: Container(
+                      height: 125,
+                      width: double.infinity,
+                      color : Colors.grey [400]
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(
+                              context,
+                                MaterialPageRoute(builder: (context) =>  const LoginScreen())
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.brown,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 49.0, vertical: 10.0),
+                            child: Center(
+                              child: Text(
+                                'Back',
+                                style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                                MaterialPageRoute(builder: (context) => const ScreensPitogo2())
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.brown,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 49, vertical: 10.0),
+                            child: Center(
+                              child: Text(
+                                'Next',
+                                style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ]
+            ),
+          ),
+        )
+      )
+    );
+  }
+}
+
+class ScreensPitogo2 extends StatelessWidget {
+  const ScreensPitogo2({super.key});
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+            child: Column(
+              children: [
+                Row( 
+                    children:[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                        child: Container(
+                          height: 97,
+                          width: 97,
+                          color : Colors.grey[400]
+                        ),
+                      ), 
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                        child: Container(
+                          height: 97,
+                          width: 97,
+                          color : Colors.grey[600]
+                        ),
+                      ), 
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                        child: Container(
+                          height: 97,
+                          width: 97,
+                          color : Colors.grey[400]
+                        ),
+                      ),
+                  ]
+                  
+                ),
+                Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(
+                              context,
+                                MaterialPageRoute(builder: (context) =>  const ScreensPitogo1())
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.brown,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 10.0),
+                            child: Center(
+                              child: Text(
+                                'Back',
+                                style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                                MaterialPageRoute(builder: (context) => const ScreensPitogo3())
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.brown,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 10.0),
+                            child: Center(
+                              child: Text(
+                                'Next',
+                                style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        )
+      )
+    );
+  }
+}
+
+class ScreensPitogo3 extends StatelessWidget {
+  const ScreensPitogo3({super.key});
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40.0),
+          child: Column( 
+              children:[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                      child: Container(
+                        height: 105,
+                        width: 105,
+                        color : Colors.grey[400]
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                      child: Container(
+                        height: 105,
+                        width: 105,
+                        color : Colors.grey[600]
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
+                      child: Container(
+                        height: 105,
+                        width: 105,
+                        color : Colors.grey[400]
+                      ),
+                    )
+                  ],
+                ), 
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30.0),
+                  child: Container(
+                    height: 125,
+                    width: double.infinity,
+                    color : Colors.grey[400]
+                  ),
+                ), 
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30.0),
+                  child: Container(
+                    height: 125,
+                    width: double.infinity,
+                    color : Colors.grey[600]
+                  ),
+                ),
+                Row( 
+                    children:[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(
+                              context
+                               // MaterialPageRoute(builder: (context) =>  const ScreensPitogo2())
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.brown,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 137, vertical: 10.0),
+                            child: Center(
+                              child: Text(
+                                'Back',
+                                style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ]
+                ),
+              ]
+          ),
+        )
+      )
     );
   }
 }
